982d2b062d4823af603cf8ce8781956f7db74d45
usermuser <lwrdrstv@gmail.com>
2020-05-20 19:08:00 +0300

    master: BaseClient update, settings update

--- api_clients/base.py
+++ api_clients/base.py
@@ -4,15 +4,15 @@ import logging
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-from settings import TOKEN
 
-from rpsos.settings import (
+from settings import (
     RETRY_COUNT,
     RETRY_TIMEOUT,
     RETRY,
     REPEAT_TIMEOUT,
     RETRY_CODES,
-    JWT_DEFAULT_TOKEN
+    TOKEN,
+    API_ENDPOINT,
 )
 
 
@@ -26,11 +26,29 @@ class BaseClient:
 
     def __init__(
             self,
-            base_url=None,
+            base_url=API_ENDPOINT,
             token=TOKEN,
             retry_count=RETRY_COUNT,
             retry_timeout=RETRY_TIMEOUT,
             retry=RETRY,
             repeat_timeout=REPEAT_TIMEOUT,
             retry_codes=RETRY_CODES,
-            ):
+    ):
+
+    def _retry_stragety(self):
+        retry_methods = ("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS", "TRACE")
+        return Retry(
+            total=self.retry_count,
+            backoff_factor=self.repeat_timeout,
+            status=self.retry_count,
+            status_forcelist=self.retry_codes,
+            connect=self.retry_count,
+            read=self.retry_count,
+            method_whitelist=retry_methods,
+        )
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
--- settings.py
+++ settings.py
@@ -1,8 +1,16 @@
 import os
 from sensitive_settings import (
-TOKEN,
-API_ENDPOINT,
-LOGIN,
-PASSWORD,
+    TOKEN,
+    API_ENDPOINT,
+    LOGIN,
+    PASSWORD,
 )
+
 BASE_DIR = os.getcwd()
+
+# RETRY STRATEGY
+RETRY_COUNT = 3
+RETRY_TIMEOUT = 3
+RETRY = True
+REPEAT_TIMEOUT = 5
+RETRY_CODES = [413, 429, 500, 502, 503, 504]
